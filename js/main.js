34751a6f676d83efa60bbf7fdd249db18e397b48
namYeJi9q <nyj5850@naver.com>
2023-03-30 12:15:54 +0900

    Inner contents

--- js/main.js
+++ js/main.js
@@ -2,14 +2,30 @@ const frame = document.querySelector("section");
 const list = frame.querySelectorAll("article");
 const prev = document.querySelector(".btnPrev");
 const next = document.querySelector(".btnNext");
+const names = [
+    "Calm",
+    "Escape",
+    "music_Blizzards",
+    "music_Dusty_Road",
+    "music_Payday",
+    "music_Retreat",
+    "Seasonal",
+    "Vespers",
+];
 const len = list.length;
 const deg = 360 / len;
 let num = 0;
 let active = 0;
 
-for (let i = 0; i < len; i++) {
-    list[i].style.transform = `rotate(${deg * i}deg) translateY(-100vh)`;
-}
+names.forEach((name, index) => {
+    const pic = list[index].querySelector(".pic");
+    const h2 = list[index].querySelector(".txt h2");
+    list[index].style.transform = `rotate(${
+        deg * index
+    }deg) translateY(-100vh)`;
+    pic.style.backgroundImage = `url(img/${name}.jpeg)`;
+    h2.innerText = name;
+});
 
 prev.addEventListener("click", (e) => {
     frame.style.transform = `rotate(${deg * ++num}deg)`;
@@ -32,3 +48,23 @@ next.addEventListener("click", (e) => {
     }
     list[active].classList.add("on");
 });
+
+for (let el of list) {
+    const play = el.querySelector(".play");
+    const pause = el.querySelector(".pause");
+    const load = el.querySelector(".load");
+
+    play.addEventListener("click", (e) => {
+        e.currentTarget
+            .closest("article")
+            .querySelector(".pic")
+            .classList.add("on");
+    });
+
+    pause.addEventListener("click", (e) => {
+        e.currentTarget
+            .closest("article")
+            .querySelector(".pic")
+            .classList.remove("on");
+    });
+}
